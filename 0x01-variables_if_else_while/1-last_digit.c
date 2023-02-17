fac1f4486b83c76fbf06437b9921d75e23073ff1
Musa Yahaya <musaybappah@gmail.com>
2023-02-17 06:21:08 -0800

    fix betty in find and print the last digit of the given number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			       	n, lastDigit);
+				n, lastDigit);
 	return (0);
 }
