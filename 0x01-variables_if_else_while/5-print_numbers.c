c4b7992e8b1095be80858c5770c2865d2f5069f5
Musa Yahaya <musaybappah@gmail.com>
2023-02-17 14:46:25 -0800

    Print numbers of base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers of base ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d\n", number);
+		number++;
+	}
+	return (0);
+}
