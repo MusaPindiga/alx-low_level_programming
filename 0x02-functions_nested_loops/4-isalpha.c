471007c06a6ce6ab27d159cbbf84cd6f20374fd6
Musa Yahaya <musaybappah@gmail.com>
2023-02-21 18:38:58 -0800

    4-isalpha.c, updated main.h and fix betty standard in 3-islower.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _islower - check the alphabet if it is a lowercase
+ * _isalpha - check the alphabet if it is a lowercase
  *
  * @c: is the parameter of the function
  *
  * Return: 0 if the alphabet is not lowercase.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
