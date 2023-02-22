8d8e83a902cc47ff6b768a7b5fbc6dc27d344b76
Musa Yahaya <musaybappah@gmail.com>
2023-02-21 20:32:48 -0800

    9-times_table.c and updated main.h file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - a funtion that print times table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+			printf("%d, ", i * j);
+
+		printf("\n");
+	}
+}
