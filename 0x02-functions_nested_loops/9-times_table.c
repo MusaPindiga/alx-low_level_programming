da5bb2893be8a081062cc4975fdbd868d9c8eb4d
Musa Yahaya <musaybappah@gmail.com>
2023-02-21 20:40:39 -0800

    Hot ifx

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,26 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
-			printf("%d, ", i * j);
+		_putchar('0');
 
-		printf("\n");
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
