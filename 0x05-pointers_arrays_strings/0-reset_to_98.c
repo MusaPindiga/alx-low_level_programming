ba6e46e761a83026a9751c4543731335bc085abb
Musa Yahaya <musaybappah@gmail.com>
2023-02-27 03:05:31 -0800

    0-reset_to_98.c and main.h files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a given argument to 98.
+ *
+ * @n: variable parameter of pointer type.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
