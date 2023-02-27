94e2153ba8f6c03d54ff243637de74e6e9e6a89e
Musa Yahaya <musaybappah@gmail.com>
2023-02-27 03:55:10 -0800

    2-strlen.c and main.h files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of a given string.
+ *
+ * @s: variable parameter of pointer type.
+ *
+ * Return: return the length of the string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		i = i + 1;
+
+	return (i);
+}
