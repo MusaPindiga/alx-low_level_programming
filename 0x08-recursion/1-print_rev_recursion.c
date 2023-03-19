ad7a92b6119e473daf7a4476ee4ff5cba5c76437
Musa Yahaya <musaybappah@gmail.com>
2023-03-19 05:52:54 -0700

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * -print_rev_recursion - function that print string characters
+ *			in reverse order
+ * @s : string characters to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
