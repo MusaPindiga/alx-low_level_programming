b654c3d14f83f7d11287a8191c94c8593dc1eabe
Musa Yahaya <musaybappah@gmail.com>
2023-03-23 04:29:42 -0700

    execute call-back on each given array element of a given size

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a given function as parameter on array
+ *
+ * @array: given array
+ * @size: size of the array
+ * @action: pointer to a function that execute on each element of
+ *	of the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int index;
+
+	for (index = 0; index < size; index++)
+		action(array[index]);
+}
