d82bbe6d610f54159248f45f1db3ad9c778d53e9
Musa Yahaya <musaybappah@gmail.com>
2023-03-23 16:27:15 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARAIDIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
