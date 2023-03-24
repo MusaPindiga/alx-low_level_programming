d0af03f1a80cbe12ad926a3399a0c64d17be2b48
Musa Yahaya <musaybappah@gmail.com>
2023-03-23 17:07:34 -0700

    sum variable number of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
  * sum_them_all - sum all the variable number of arguments
  *
  * @n: the fixed parameter variable
+ * @...: varaible number of arguments tu be sum up
  *
  * Return: 0 if n == 0, otherwise sum of the arguments
  */
@@ -14,9 +15,6 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int index, sum = 0;
 
 	va_start(nums, n);
-
-	if (n == 0)
-		return (0);
 
 	for (index = 0; index < n; index++)
 		sum += va_arg(nums, int);
