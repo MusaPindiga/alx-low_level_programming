4f879e012004e01a199c00f011b438160b82669c
Musa <musaybappah@gmail.com>
2023-06-09 11:27:49 -0700

    function that print the name of  the executable file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function that print the name of the
+ *	file (executable).
+ * @argc: argument counter parameter variable
+ * @argv: argument vector parameter variable.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
