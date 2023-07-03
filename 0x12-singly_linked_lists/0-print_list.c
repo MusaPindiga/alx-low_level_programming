7a417c4329e17e2a6afb8f8102bb5e5d4d9b69f4
Musa <musaybappah@gmail.com>
2023-07-03 07:14:23 -0700

    fix some anonymous issues in print_list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_list - print the list of list_t
- * @h: list of type list_t to be printed
- * Return: print [0] (nil) if str (of list_t) == NULL
- *		number of nodes in list_t otherwise.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
@@ -16,10 +16,13 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
-			printf("[%d] %s \n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
+
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
 }
