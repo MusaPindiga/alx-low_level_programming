7f401b5bda5fee97c03cbd45edcf595afaaa973e
Musa Yahaya <musaybappah@gmail.com>
2023-08-18 15:44:46 -0700

    finds the length of the list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - finds the length of a list
+ * @h: the list to finds its length
+ *
+ * Return: length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
